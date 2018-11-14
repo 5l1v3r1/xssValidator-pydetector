b812255e90074a542c0c209df88f089ba9a2cee2
Roberto Salgado <lightos@gmail.com>
2018-11-13 21:57:29 -0800

    Update xss.py

--- xss.py
+++ xss.py
@@ -11,7 +11,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from splinter import Browser
 
 
-PORT = 8094
+PORT = 8093
 user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0)"\
     "Gecko/20100101 Firefox/45.1"
 chrome_options = webdriver.ChromeOptions()
@@ -45,7 +45,6 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             s.send_header("Content-type", "text/json")
             s.end_headers()
 
-
 def probe(data):
     # Normalize data
     data = [_.split("=") for _ in data.split("&")]
@@ -54,13 +53,10 @@ def probe(data):
     http_response = unquote(data[0][1])
 
     print http_url
-    print
 
     html_response = "var page=atob('%s');page=page.substr(page.indexOf('\\n\\n'), page.length).trim();top.document.write(page);" % http_response
-    xssDetection = '''var tags = ["a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "audioscope", "b", "base", "basefont", "bdi", "bdo", "bgsound", "big", "blackface", "blink", "blockquote", "body", "bq", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "command", "comment", "datalist", "dd", "del", "details", "dfn", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "fn", "font", "footer", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "ilayer", "img", "input", "ins", "isindex", "kbd", "keygen", "label", "layer", "legend", "li", "limittext", "link", "listing", "map", "mark", "marquee", "menu", "meta", "meter", "multicol", "nav", "nobr", "noembed", "noframes", "noscript", "nosmartquotes", "object", "ol", "optgroup", "option", "output", "p", "param", "plaintext", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "server", "shadow", "sidebar", "small", "source", "spacer", "span", "strike", "strong", "style", "sub", "sup", "table", "tbody", "td", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "tt", "u", "ul", "var", "video", "wbr", "xml", "xmp"];
-var eventHandler = ["mousemove", "mouseout", "mouseover"];
-      tags.forEach(function(tag) {
-        currentTags = document.querySelectorAll(tag);
+    xssDetection = '''var eventHandler = ["mousemove", "mouseout", "mouseover"];
+        currentTags = document.getElementsByTagName("*");
         if (currentTags !== null && currentTags.length > 0) {
           for (i = 0; i < currentTags.length; i++) {
             eventHandler.forEach(function(currentEvent) {
@@ -69,27 +65,39 @@ var eventHandler = ["mousemove", "mouseout", "mouseover"];
               currentTags[i].dispatchEvent(ev);
             });
           }
-        }
-      });'''
+        }'''
 
-    def xss_detected():
-        print "XSS found!"
+    def accept_alert():
+        # Try to handle payloads which trigger multiple alerts
+        try:
+            with browser.get_alert() as alert:
+                        alert.accept()
+            accept_alert()
+        except:
+            pass
 
     def verify_xss():
         try:
-            prompt = browser.get_alert()
-            prompt.accept()
-            xss_detected()
+            browser.get_alert().accept()
+            print "XSS found!\n"
             return True
         except UnexpectedAlertPresentException:
-            pass#print "UNEXPECTED XSS"
+            pass #print "UNEXPECTED XSS"
         except NoAlertPresentException:
-            pass#print "NO XSS DETECTED"
+            print #"NO XSS DETECTED"
         except:
             pass
         return False
     # Use dummy page for testing
-    browser.visit("about:config")
+
+    try:
+        browser.visit("chrome://dino")
+    except UnexpectedAlertPresentException:
+        # Alert(s) didn't close from last page
+        accept_alert()
+        browser.visit("chrome://dino")
+
+    #raw_input()
     #browser.visit(http_url)
     # Load HTML Response onto dummy page
     browser.execute_script(html_response)
